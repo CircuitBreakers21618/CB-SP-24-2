768947e0bb2855301092ab2841e695c066a81fa4
Circut21618 <circuitbreakers21618@gmail.com>
2024-11-04 20:09:14 -0700

    fix the caling of classses

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive2.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Drive2 {
+
+    public DcMotor topleft;
+    public DcMotor topright;
+    public DcMotor backleft;
+    public DcMotor backright;
+
+    public void init(HardwareMap hwMap){
+
+        topleft = hwMap.get(DcMotor.class, "topleft");
+        topright = hwMap.get(DcMotor.class, "topright");
+        backleft = hwMap.get(DcMotor.class, "backleft");
+        backright = hwMap.get(DcMotor.class, "backright");
+    }
+}
